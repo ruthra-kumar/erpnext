5dcd24571704b957d07cbeef68f40ed91f88d16c
ruthra kumar <ruthra@erpnext.com>
2024-04-16 17:14:49 +0530

    refactor: add filters on UI side

--- erpnext/accounts/report/tds_and_tcs_breakdown/tds_and_tcs_breakdown.js
+++ erpnext/accounts/report/tds_and_tcs_breakdown/tds_and_tcs_breakdown.js
@@ -2,5 +2,41 @@
 // For license information, please see license.txt
 
 frappe.query_reports["TDS and TCS Breakdown"] = {
-	filters: [],
+	filters: [
+		{
+			fieldname: "company",
+			label: __("Company"),
+			fieldtype: "Link",
+			options: "Company",
+			default: frappe.defaults.get_default("company"),
+		},
+		{
+			fieldname: "voucher_type",
+			label: __("Voucher Type"),
+			fieldtype: "Select",
+			options: ["Sales Invoice", "Purchase Invoice", "Journal Entry"],
+			reqd: 1,
+			default: "Sales Invoice",
+		},
+		{
+			fieldname: "voucher_no",
+			label: __("Voucher No"),
+			fieldtype: "Dynamic Link",
+			get_options: function () {
+				var voucher_type = frappe.query_report.get_filter_value("voucher_type");
+				if (!voucher_type) {
+					frappe.throw(__("Please select Voucher Type first"));
+				}
+				return voucher_type;
+			},
+			get_query: function () {
+				var company = frappe.query_report.get_filter_value("company");
+				return {
+					filters: {
+						company: company,
+					},
+				};
+			},
+		},
+	],
 };
