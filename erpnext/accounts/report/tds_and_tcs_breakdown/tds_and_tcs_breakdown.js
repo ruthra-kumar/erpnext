412398e38c99e30a9c545346978fbd2064c0834f
ruthra kumar <ruthra@erpnext.com>
2024-04-16 17:02:42 +0530

    feat: introduce a new report for TCS/TDS breakdown

---
+++ erpnext/accounts/report/tds_and_tcs_breakdown/tds_and_tcs_breakdown.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2024, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["TDS and TCS Breakdown"] = {
+	filters: [],
+};
